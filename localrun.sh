0dbac8ee652d988de267f5d725d847cf267c1cdc
Gregory Petrosyan <pgregory@pgregory.net>
2022-12-15 23:50:18 +0300

    localrun: open live view

--- localrun.sh
+++ localrun.sh
@@ -7,7 +7,7 @@ echo -n Waiting for services to be ready...
 until docker exec sh-aggregator-kh clickhouse-client --query='SELECT 1' >/dev/null 2>&1; do echo -n .; sleep 0.2; done
 until curl --output /dev/null --silent --head --fail http://localhost:10888/; do echo -n .; sleep 0.2; done
 echo READY
-URL="http://localhost:10888/view?f=-300&t=0&s=__contributors_log_rev"
+URL="http://localhost:10888/view?live&f=-300&t=0&s=__contributors_log_rev"
 case "$OSTYPE" in
   darwin*)  open "$URL" ;;
   linux*)   xdg-open "$URL" ;;
