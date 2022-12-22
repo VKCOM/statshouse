6b8b4623c9242b0c61c1f3e7b9eaad6f7bcf4705
Mikhail <m.alpinskiy@vk.team>
2022-12-22 22:47:31 +0300

    New tag name (numbers only) handler (#94)
    fixes OG image renderer
    
    Co-authored-by: Mikhail Alpinskiy <Mikhail Alpinskiy>

--- internal/api/httputil.go
+++ internal/api/httputil.go
@@ -336,6 +336,9 @@ func parseTagID(tagID string) (string, error) {
 	if tagID == format.StringTopTagID || format.ParseTagIDForAPI(tagID) >= 0 {
 		return tagID, nil
 	}
+	if i, err := strconv.Atoi(tagID); err == nil && 0 <= i && i < format.MaxTags {
+		return format.TagID(i), nil
+	}
 	return "", httpErr(http.StatusBadRequest, fmt.Errorf("invalid tag ID: %q", tagID))
 }
 
