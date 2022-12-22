0ae57421b222362c69954620a7cf92b608b74b1a
Mikhail Alpinskiy <Mikhail Alpinskiy>
2022-12-22 15:29:31 +0300

    Docker compose profile for API debugging
    * Start StatsHouse (except API), grafana and prometheus with
      ./localrun api-dev
    * Prom scrapes demo.promlabs.com server and sends data to StatsHouse
    * Run StatsHouse API under debugger with command line below
      --verbose --local-mode --access-log --clickhouse-v1-addrs= --clickhouse-v2-addrs=localhost:9000 --listen-addr=:10888 --metadata-addr=localhost:2442 --statshouse-addr=localhost:13337 --disk-cache=<disk_cache_path> --static-dir=statshouse-ui/build
    * Open http://localhost:10888/view?live=&f=-300&t=0&s=__contributors_log_rev

--- localrun.sh
+++ localrun.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 set -e
 
-docker compose --profile sh up -d --build
-trap "{ docker compose --profile sh down; exit; }" exit
+PROFILE=sh
+if [[ $1 ]]; then
+  PROFILE=$1
+fi
+
+docker compose --profile "$PROFILE" up -d --build
+trap "{ docker compose --profile $PROFILE down; exit; }" exit
 echo -n Waiting for services to be ready...
-until docker exec sh-aggregator-kh clickhouse-client --query='SELECT 1' >/dev/null 2>&1; do echo -n .; sleep 0.2; done
-until curl --output /dev/null --silent --head --fail http://localhost:10888/; do echo -n .; sleep 0.2; done
+until docker exec kh clickhouse-client --query='SELECT 1' >/dev/null 2>&1; do echo -n .; sleep 0.2; done
+if [ "$(docker container inspect -f '{{.State.Status}}' sh-api 2>/dev/null)" = "running" ]; then
+  until curl --output /dev/null --silent --head --fail http://localhost:10888/; do echo -n .; sleep 0.2; done
+  URL="http://localhost:10888/view?live&f=-300&t=0&s=__contributors_log_rev"
+  case "$OSTYPE" in
+    darwin*)  open "$URL" ;;
+    linux*)   xdg-open "$URL" ;;
+  esac
+fi
 echo READY
-URL="http://localhost:10888/view?live&f=-300&t=0&s=__contributors_log_rev"
-case "$OSTYPE" in
-  darwin*)  open "$URL" ;;
-  linux*)   xdg-open "$URL" ;;
-esac
 read -r -p "Press ENTER key or CTRL+C to exit."
