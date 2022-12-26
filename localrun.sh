ce81f8b3371d3d6c88218f1389b7e079374f8857
Mikhail Alpinskiy <Mikhail Alpinskiy>
2022-12-26 18:59:11 +0300

    Fixed localrun

--- localrun.sh
+++ localrun.sh
@@ -9,7 +9,11 @@ fi
 docker compose --profile "$PROFILE" up -d --build
 trap "{ docker compose --profile $PROFILE down; exit; }" exit
 echo -n Waiting for services to be ready...
-until docker exec kh clickhouse-client --query='SELECT 1' >/dev/null 2>&1; do echo -n .; sleep 0.2; done
+for c in kh all-in-one; do
+  if [ "$(docker container inspect -f '{{.State.Status}}' $c 2>/dev/null)" = "running" ]; then
+    until docker exec $c clickhouse-client --query='SELECT 1' >/dev/null 2>&1; do echo -n .; sleep 0.2; done
+  fi
+done
 if [ "$(docker container inspect -f '{{.State.Status}}' sh-api 2>/dev/null)" = "running" ]; then
   until curl --output /dev/null --silent --head --fail http://localhost:10888/; do echo -n .; sleep 0.2; done
   URL="http://localhost:10888/view?live&f=-300&t=0&s=__contributors_log_rev"
