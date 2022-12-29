f3941f261c5cf40de0b865651d745cda9f4fa96d
Mikhail Alpinskiy <Mikhail Alpinskiy>
2022-12-30 01:05:04 +0300

    New localrun configurations
    Three ./localrun modes are now available:
    * `./localrun api-off`
    * `./localrun agent-off`
    * `./localrun aggregator-off`
    
    They runs StatsHouse without component specified before `-off` part of the argument. All running services are exposed to `localhost`, so you can run 'missing' service on host machine and connect to the rest StatsHouse running inside ./localrun invocation. It's a convinient way to debug a part of StatsHouse.
    
    Three corresponding GoLand run configurations are added:
    * api
    * aggregator
    * agent
    
    which are configured to connect to services running inside ./localrun invocation.
    
    Works brilliant on my machine. Happy debugging!

--- build/all-in-one-entrypoint.sh
+++ build/all-in-one-entrypoint.sh
@@ -3,11 +3,15 @@ set -e
 /entrypoint.sh &
 /bin/statshouse-metadata --db-path=/var/lib/statshouse/metadata/db --binlog-prefix=/var/lib/statshouse/metadata/binlog/bl &
 until clickhouse-client --query="SELECT 1"; do sleep 0.2; done
-/bin/statshouse aggregator --cluster=test_shard_localhost --log-level=trace --agg-addr=':13336' --kh=127.0.0.1:8123 \
-  --auto-create --cache-dir=/var/lib/statshouse/cache/aggregator -u=root -g=root &
-/bin/statshouse agent --cluster=test_shard_localhost --log-level=trace --remote-write-enabled \
-  --agg-addr='127.0.0.1:13336,127.0.0.1:13336,127.0.0.1:13336' --cache-dir=/var/lib/statshouse/cache/agent \
-  -u=root -g=root &
+if [ -z "$AGGREGATOR_OFF" ]; then
+  /bin/statshouse aggregator --cluster=test_shard_localhost --log-level=trace --agg-addr=':13336' --kh=127.0.0.1:8123 \
+    --auto-create --cache-dir=/var/lib/statshouse/cache/aggregator -u=root -g=root &
+fi
+if [ -z "$AGENT_OFF" ]; then
+  /bin/statshouse agent --cluster=test_shard_localhost --log-level=trace --remote-write-enabled \
+    --agg-addr='127.0.0.1:13336,127.0.0.1:13336,127.0.0.1:13336' --cache-dir=/var/lib/statshouse/cache/agent \
+    -u=root -g=root &
+fi
 /bin/statshouse-api --verbose --local-mode --access-log --clickhouse-v1-addrs= --clickhouse-v2-addrs=127.0.0.1:9000 \
   --listen-addr=:10888 --statshouse-addr=127.0.0.1:13337 --disk-cache=/var/lib/statshouse/cache/api/mapping_cache.sqlite3 \
   --static-dir=/usr/lib/statshouse-api/statshouse-ui/ &
