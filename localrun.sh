870746a1237f1cd3ba077ec8b5e6ba8201985778
Mikhail <m.alpinskiy@vk.team>
2022-12-30 01:49:41 +0300

    New localrun configurations (#104)
    Three ./localrun modes are now available:
    * `./localrun api-off`
    * `./localrun agent-off`
    * `./localrun aggregator-off`
    
    They runs StatsHouse without component specified before `-off` part of the argument. All running services are exposed to `localhost`, so you can run 'missing' service on host machine and connect to the rest StatsHouse running inside ./localrun invocation. It's a convinient way to debug a part of StatsHouse.
    
    Three corresponding GoLand run configurations are added:
    * api
    * aggregator
    * agent
    
    which are configured to connect to services running inside ./localrun invocation.
    
    Works brilliant on my machine. Happy debugging!
    
    Co-authored-by: Mikhail Alpinskiy <Mikhail Alpinskiy>

--- localrun.sh
+++ localrun.sh
@@ -9,14 +9,14 @@ fi
 docker compose --profile "$PROFILE" up -d --build
 trap "{ docker compose --profile $PROFILE down; exit; }" exit
 echo -n Waiting for services to be ready...
-for c in kh sh-all-in-one; do
+for c in kh sh; do
   if [ "$(docker container inspect -f '{{.State.Status}}' $c 2>/dev/null)" = "running" ]; then
     until docker exec $c clickhouse-client --query='SELECT 1' >/dev/null 2>&1; do echo -n .; sleep 0.2; done
     until curl --output /dev/null --silent --head --fail http://localhost:8123/; do echo -n .; sleep 0.2; done
     break
   fi
 done
-for c in sh-api sh-all-in-one; do
+for c in sh sh-api; do
   if [ "$(docker container inspect -f '{{.State.Status}}' $c 2>/dev/null)" = "running" ]; then
     until curl --output /dev/null --silent --head --fail http://localhost:10888/; do echo -n .; sleep 0.2; done
     URL="http://localhost:10888/view?live&f=-300&t=0&s=__contributors_log_rev"
