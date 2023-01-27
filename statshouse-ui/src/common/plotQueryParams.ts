eafc7d9f1155f39cd2fc9bd7ee829fb27819fe61
Anton Ulyanov <polecat5@yandex.ru>
2023-01-27 15:19:41 +0300

    StatsHouse UI: hot fix param (#222)

--- statshouse-ui/src/common/plotQueryParams.ts
+++ statshouse-ui/src/common/plotQueryParams.ts
@@ -226,6 +226,7 @@ export const configParams: ConfigParams = {
         isArray: true,
       },
       customAgg: {
+        ...NumberParam,
         urlKey: queryParamAgg,
         default: 0,
       },
