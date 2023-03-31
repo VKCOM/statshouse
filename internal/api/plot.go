addae02f60f02f1f896bd317e2762b76cf6d67a7
Mikhail <m.alpinskiy@vk.team>
2023-03-31 19:25:55 +0300

    Sync GNUPlot palette with TypeScript code

--- internal/api/plot.go
+++ internal/api/plot.go
@@ -93,17 +93,87 @@ unset multiplot
 
 var (
 	// XXX: keep in sync with TypeScript
-	palette = [...][2]string{
-		{"blue", "#0d6efd"},   // blue
-		{"purple", "#6610f2"}, // indigo
-		{"purple", "#6f42c1"}, // purple
-		{"red", "#d63384"},    // pink
-		{"red", "#dc3545"},    // red
-		{"yellow", "#fd7e14"}, // orange
-		{"yellow", "#ffc107"}, // yellow
-		{"green", "#198754"},  // green
-		{"green", "#20c997"},  // teal
-		{"blue", "#0dcaf0"},   // cyan
+	palette = [...][]string{
+		{
+			"blue",
+			"#0d6efd", // blue-500
+			"#6ea8fe", // blue-300
+			"#084298", // blue-700
+			"#3d8bfd", // blue-400
+			"#0a58ca", // blue-600
+		},
+		{
+			"purple",
+			"#6610f2", // indigo-500
+			"#a370f7", // indigo-300
+			"#3d0a91", // indigo-700
+			"#8540f5", // indigo-400
+			"#520dc2", // indigo-600
+		},
+		{
+			"purple",
+			"#6f42c1", // purple-500
+			"#a98eda", // purple-300
+			"#432874", // purple-700
+			"#8c68cd", // purple-400
+			"#59359a", // purple-600
+		},
+		{
+			"red",
+			"#d63384", // pink-500
+			"#e685b5", // pink-300
+			"#801f4f", // pink-700
+			"#de5c9d", // pink-400
+			"#ab296a", // pink-600
+		},
+		{
+			"red",
+			"#dc3545", // red-500
+			"#ea868f", // red-300
+			"#842029", // red-700
+			"#e35d6a", // red-400
+			"#b02a37", // red-600
+		},
+		{
+			"yellow",
+			"#fd7e14", // orange-500
+			"#feb272", // orange-300
+			"#984c0c", // orange-700
+			"#fd9843", // orange-400
+			"#ca6510", // orange-600
+		},
+		{
+			"yellow",
+			"#ffc107", // yellow-500
+			"#ffda6a", // yellow-300
+			"#997404", // yellow-700
+			"#ffda6a", // yellow-400
+			"#cc9a06", // yellow-600
+		},
+		{
+			"green",
+			"#198754", // green-500
+			"#75b798", // green-300
+			"#0f5132", // green-700
+			"#479f76", // green-400
+			"#146c43", // green-600
+		},
+		{
+			"green",
+			"#20c997", // teal-500
+			"#79dfc1", // teal-300
+			"#13795b", // teal-700
+			"#4dd4ac", // teal-400
+			"#1aa179", // teal-600
+		},
+		{
+			"blue",
+			"#0dcaf0", // cyan-500
+			"#6edff6", // cyan-300
+			"#087990", // cyan-700
+			"#3dd5f3", // cyan-400
+			"#0aa2c0", // cyan-600
+		},
 	}
 )
 
@@ -154,7 +224,10 @@ func (d *gnuplotTemplateData) Header() string {
 }
 
 func (d *gnuplotTemplateData) LineColor(meta QuerySeriesMetaV2) string {
-	s := fmt.Sprintf("%s: %s", d.Metric, d.MetaToLabel(meta))
+	var (
+		prefColor = 9 // it`s magic prefix (copied from TypeScript code)
+		s         = fmt.Sprintf("%d%s", prefColor, d.MetaToLabel(meta))
+	)
 	return selectColor(s, d.usedColorIndices)
 }
 
