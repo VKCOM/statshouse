1d7811516a672f8fc3581b33114f25daa95bad2e
Evgeny Martyn <dedok1486@gmail.com>
2023-04-26 17:52:33 +0300

    [api] fix skey (#395)
    Co-authored-by: e.martyn <e.martyn@emartyn-nb.local>

--- internal/api/httputil.go
+++ internal/api/httputil.go
@@ -358,6 +358,9 @@ func parseTagID(tagID string) (string, error) {
 	if tagID == format.StringTopTagID || format.ParseTagIDForAPI(tagID) >= 0 {
 		return tagID, nil
 	}
+	if tagID == format.NewStringTopTagID {
+		return format.StringTopTagID, nil
+	}
 	if i, err := strconv.Atoi(tagID); err == nil && 0 <= i && i < format.MaxTags {
 		return format.TagID(i), nil
 	}
