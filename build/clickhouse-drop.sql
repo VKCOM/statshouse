4e1b4cf52bc796bf933f4979bcf457873e3a0b44
Grigory Buteyko <hrissan@hrissan.com>
2023-05-24 17:53:01 +0300

    upgraded statshouse-go lib

---
+++ build/clickhouse-drop.sql
@@ -0,0 +1,23 @@
+-- for simplicity should contains all tables from all scripts. For now contains table from clickhouse.sql
+DROP TABLE IF EXISTS statshouse_value_incoming_prekey;
+DROP TABLE IF EXISTS statshouse_value_1s_dist;
+DROP TABLE IF EXISTS statshouse_value_1s_prekey_dist;
+DROP TABLE IF EXISTS statshouse_value_1s_agg2;
+DROP TABLE IF EXISTS statshouse_value_1s_agg_prekey;
+DROP TABLE IF EXISTS statshouse_value_1m_dist;
+DROP TABLE IF EXISTS statshouse_value_1m_prekey_dist;
+DROP TABLE IF EXISTS statshouse_value_1m_agg2;
+DROP TABLE IF EXISTS statshouse_value_1m_agg_prekey;
+DROP TABLE IF EXISTS statshouse_value_1h_dist;
+DROP TABLE IF EXISTS statshouse_value_1h_prekey_dist;
+DROP TABLE IF EXISTS statshouse_value_1h_agg2;
+DROP TABLE IF EXISTS statshouse_value_1h_agg_prekey;
+DROP TABLE IF EXISTS statshouse_contributors_log_dist;
+DROP TABLE IF EXISTS statshouse_contributors_log_buffer;
+DROP TABLE IF EXISTS statshouse_contributors_log_agg2;
+DROP TABLE IF EXISTS statshouse_metric_1d_dist;
+DROP TABLE IF EXISTS statshouse_metric_1d_buffer;
+DROP TABLE IF EXISTS statshouse_metric_1d_agg2;
+DROP TABLE IF EXISTS statshouse_internal_log_dist;
+DROP TABLE IF EXISTS statshouse_internal_log_buffer;
+
