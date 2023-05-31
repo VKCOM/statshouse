abfd16d55e30eb1f93bc20f90f31a52d0868a452
Gregory Petrosyan <pgregory@pgregory.net>
2023-05-31 18:24:40 +0300

    sqlite: simplify prepare() params

--- internal/sqlite/conn.go
+++ internal/sqlite/conn.go
@@ -252,7 +252,7 @@ func (c Conn) doQuery(isRO, allowUnsafe bool, sqlBytes []byte, sqlString string,
 	si, ok := c.c.cache.get(sqlString, sqlBytes)
 	if !ok {
 		start := time.Now()
-		si, err = prepare(c.c.rw, sqlBytes, true)
+		si, err = prepare(c.c.rw, sqlBytes, false)
 		c.stats.measureActionDurationSince("sqlite_prepare", start)
 		if err != nil {
 			return nil, err
@@ -335,8 +335,8 @@ func (c Conn) doStmt(si stmtInfo, args ...Arg) (*sqlite0.Stmt, error) {
 	return si.stmt, nil
 }
 
-func prepare(c *sqlite0.Conn, sql []byte, skipCache bool) (stmtInfo, error) {
-	s, _, err := c.Prepare(sql, !skipCache)
+func prepare(c *sqlite0.Conn, sql []byte, persistent bool) (stmtInfo, error) {
+	s, _, err := c.Prepare(sql, persistent)
 	if err != nil {
 		return stmtInfo{}, err
 	}
