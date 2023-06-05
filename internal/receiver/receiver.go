0298bae4374e929d74160dc503e7022ccc7608b9
Mikhail <m.alpinskiy@vk.team>
2023-06-06 01:30:25 +0300

    Fixed compilation on MacOS
    syscall.SO_RCVBUFFORCE is not available there

--- internal/receiver/receiver.go
+++ internal/receiver/receiver.go
@@ -112,23 +112,7 @@ func listenPacket(address string, fn func(int) error) (conn net.PacketConn, err 
 
 func ListenUDP(address string, bufferSize int, reusePort bool, bm *agent.Agent, logPacket func(format string, args ...interface{})) (*UDP, error) {
 	conn, err := listenPacket(address, func(fd int) error {
-		if syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_RCVBUFFORCE, bufferSize) != nil {
-			if syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_RCVBUF, bufferSize) != nil {
-				// Either we don't have CAP_NET_ADMIN priviledge to set SO_RCVBUFFORCE
-				// or buffer size is beyond configured system limit.
-				// Trying to set the largest value possible.
-				var curr int
-				if n, err := syscall.GetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF); err == nil {
-					curr = n
-				}
-				for bufferSize /= 2; curr < bufferSize; bufferSize /= 2 {
-					if syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_RCVBUF, bufferSize) == nil {
-						break
-					}
-
-				}
-			}
-		}
+		setSocketReceiveBufferSize(fd, bufferSize)
 		if reusePort {
 			return syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, unix.SO_REUSEPORT, 1)
 		}
