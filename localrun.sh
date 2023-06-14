860307a1a5f389497f34baad368bd02e452120df
Anton Ulyanov <polecat5@yandex.ru>
2023-06-14 11:19:25 +0300

    fix live link (#562)

--- localrun.sh
+++ localrun.sh
@@ -22,7 +22,7 @@ done
 for c in sh sh-api; do
   if [ "$(docker container inspect -f '{{.State.Status}}' $c 2>/dev/null)" = "running" ]; then
     until curl --output /dev/null --silent --head --fail http://localhost:10888/; do echo -n .; sleep 0.2; done
-    URL="http://localhost:10888/view?live&f=-300&t=0&s=__contributors_log_rev"
+    URL="http://localhost:10888/view?live=1&f=-300&t=0&s=__contributors_log_rev"
     case "$OSTYPE" in
       darwin*)  open "$URL" ;;
       linux*)   xdg-open "$URL" ;;
