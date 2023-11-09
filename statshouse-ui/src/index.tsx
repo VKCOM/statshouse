0f3d194fb41a0c1ab10eeba70929950e1c1cf540
Anton Ulyanov <polecat5@yandex.ru>
2023-11-09 13:53:56 +0300

    StatsHouse UI: feature error log

--- statshouse-ui/src/index.tsx
+++ statshouse-ui/src/index.tsx
@@ -10,6 +10,10 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppRouter } from './components';
+import { logError } from './api/stat';
+
+window.addEventListener('error', logError, false);
+
 const root = createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
