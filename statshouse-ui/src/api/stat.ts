5c74c9156a0f973a766b4c5ae1ba0daf88578c6a
Anton Ulyanov <polecat5@yandex.ru>
2023-11-09 13:53:56 +0300

    StatsHouse UI: feature error log

---
+++ statshouse-ui/src/api/stat.ts
@@ -0,0 +1,48 @@
+// Copyright 2023 V Kontakte LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import { apiFetch } from './api';
+
+const ApiStatEndpoint = '/api/stat';
+/**
+ * Response endpoint api/stat
+ */
+export type ApiStat = {};
+
+/**
+ * Get params endpoint api/stat
+ */
+export type ApiStatGet = {};
+
+/**
+ * Post params endpoint api/stat
+ */
+export type ApiStatPost = {};
+
+export async function apiStatFetch(params: ApiStatPost, keyRequest?: unknown) {
+  return await apiFetch<ApiStat>({ url: ApiStatEndpoint, post: params, keyRequest });
+}
+
+const environment = process.env.NODE_ENV;
+const UIErrorMetrickName = '__ui_errors';
+export async function logError(event: ErrorEvent) {
+  const fileName = event.filename.split('/').slice(-1)[0];
+  const message = `${fileName}:${event.lineno}:${event.colno} ${event.error?.name}: ${event.error?.message}`;
+  await apiStatFetch({
+    metrics: [
+      {
+        ts: Math.floor(Date.now() / 1000),
+        name: UIErrorMetrickName,
+        tags: {
+          0: environment,
+          _s: message,
+        },
+        counter: 1,
+        value: [1],
+      },
+    ],
+  });
+}
