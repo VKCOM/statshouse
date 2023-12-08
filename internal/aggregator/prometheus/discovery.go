debd1b93e7f280f50334c517bfeed8ee8212b16d
Mikhail <m.alpinskiy@vk.team>
2023-12-08 16:54:41 +0300

    Register k8s scrape plugin

--- internal/aggregator/prometheus/discovery.go
+++ internal/aggregator/prometheus/discovery.go
@@ -17,6 +17,7 @@ import (
 	"github.com/go-kit/log"
 	"github.com/prometheus/prometheus/config"
 	"github.com/prometheus/prometheus/discovery"
+	_ "github.com/prometheus/prometheus/discovery/kubernetes"
 )
 
 type discoveryManager struct {
