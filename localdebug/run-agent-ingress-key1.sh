03ab05e71a130df3e566863847b25bfbef6e01e4
e.martyn <dedok1486@gmail.com>
2024-01-19 12:38:08 +0300

    [sqlite] fix commit

--- localdebug/run-agent-ingress-key1.sh
+++ localdebug/run-agent-ingress-key1.sh
@@ -3,4 +3,4 @@ set -e
 
 mkdir -p cache/agent-ingress-key1
 
-../target/statshouse agent --cluster=test_shard_localhost --hostname=agent_ingress_key1 -p=13334 --agg-addr=127.0.0.1:13327,127.0.0.1:13327,127.0.0.1:13327 --log-level=trace --cache-dir=cache/agent-ingress-key1 --aes-pwd-file=ingress_keys/key1.txt "$@"
+../target/statshouse agent --cluster=local_test_cluster --hostname=agent_ingress_key1 -p=13334 --agg-addr=127.0.0.1:13327,127.0.0.1:13327,127.0.0.1:13327 --log-level=trace --cache-dir=cache/agent-ingress-key1 --aes-pwd-file=ingress_keys/key1.txt "$@"
