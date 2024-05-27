cc3fb00030851d07ad1215cc3fd30b1b1a10413b
Anton Ulyanov <polecat5@yandex.ru>
2024-05-27 19:34:32 +0300

    StatsHouse UI: fix change metric after 403 error

--- statshouse-ui/src/store/statshouse.ts
+++ statshouse-ui/src/store/statshouse.ts
@@ -781,10 +781,6 @@ export const useStore = createStoreWithEqualityFn<Store>((setState, getState, st
       );
       const prev: PlotStore = getState().plotsData[index];
 
-      if (prev.error403) {
-        return;
-      }
-
       const deltaTime = Math.floor((prevStateTo - prevStateFrom) / 5);
       if (
         !usePlotVisibilityStore.getState().visibilityList[index] &&
@@ -807,6 +803,10 @@ export const useStore = createStoreWithEqualityFn<Store>((setState, getState, st
         { metricName: lastPlotParams.metricName },
         { metricName: prev.lastPlotParams?.metricName }
       );
+      if (!changeMetric && prev.error403) {
+        return;
+      }
+
       const resetCache = !dequal(lastPlotParams, prev.lastPlotParams);
       if (
         width &&
