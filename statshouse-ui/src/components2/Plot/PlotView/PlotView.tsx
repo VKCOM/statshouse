4062ad8d622595074f08a02a06a29b780418a6b4
Anton Ulyanov <polecat5@yandex.ru>
2024-05-27 19:33:01 +0300

    StatsHouse UI: feature next state step 5

--- statshouse-ui/src/components2/Plot/PlotView/PlotView.tsx
+++ statshouse-ui/src/components2/Plot/PlotView/PlotView.tsx
@@ -4,19 +4,22 @@ import { PLOT_TYPE } from '../../../api/enum';
 import { PlotViewMetric } from './PlotViewMetric';
 import { PlotViewEvent } from './PlotViewEvent';
 export type PlotViewProps = {
+  className?: string;
   plot?: PlotParams;
   plotInfo?: PlotInfo;
   plotData?: PlotData;
 };
-export function PlotView({ plot, plotInfo, plotData }: PlotViewProps) {
+export function PlotView({ className, plot, plotInfo, plotData }: PlotViewProps) {
   // if (!plot || !plotInfo || !plotData) {
   //   return null;
   // }
   switch (plot?.type) {
     case PLOT_TYPE.Metric:
-      return <PlotViewMetric plot={plot} plotInfo={plotInfo} plotData={plotData}></PlotViewMetric>;
+      return (
+        <PlotViewMetric className={className} plot={plot} plotInfo={plotInfo} plotData={plotData}></PlotViewMetric>
+      );
     case PLOT_TYPE.Event:
-      return <PlotViewEvent plot={plot} plotInfo={plotInfo} plotData={plotData}></PlotViewEvent>;
+      return <PlotViewEvent className={className} plot={plot} plotInfo={plotInfo} plotData={plotData}></PlotViewEvent>;
     default:
       return null;
   }
