114c4915ca32583a5e3cf0b1d297eec451f49900
Sergei Razmetov <razmser@gmail.com>
2024-06-18 18:53:16 +0300

    Allow multiple clients and random tags in load gen

--- cmd/statshouse-load-generator/statshouse-load-generator.go
+++ cmd/statshouse-load-generator/statshouse-load-generator.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
@@ -14,28 +17,31 @@ import (
 	"github.com/vkcom/statshouse-go"
 )
 
-func main() {
-	var (
-		numberOfMetrics int
-		done            = make(chan os.Signal, 1)
-	)
-	flag.IntVar(&numberOfMetrics, "m", 6, "number of metrics")
-	flag.Parse()
-	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
+func randomString(rng *rand.Rand) string {
+	length := rng.Intn(100)
+	result := make([]byte, length)
+	const totalLetters = 'z' - 'A'
+	for i := 0; i < length; i++ {
+		result[i] = 'A' + byte(rng.Intn(totalLetters))
+	}
+	return string(result)
+}
 
-	client := http.Client{}
-	ensureDashboardExists(client, numberOfMetrics)
-
+func randomWalk(ctx context.Context, client *statshouse.Client, tags statshouse.NamedTags, metricsN int, randomTag bool) {
 	ticker := time.NewTicker(time.Second)
-	values := make([]float64, numberOfMetrics)
+	values := make([]float64, metricsN)
 	rng := rand.New()
 	for {
 		select {
-		case <-done:
+		case <-ctx.Done():
 			return
 		case <-ticker.C:
-			for m := 0; m < numberOfMetrics; m++ {
-				statshouse.MetricNamed(fmt.Sprint(randomWalkPrefix, m), statshouse.NamedTags{}).Value(values[m])
+			for m := 0; m < metricsN; m++ {
+				metricTags := tags
+				if randomTag {
+					metricTags = append(metricTags, [2]string{"random", randomString(rng)})
+				}
+				client.MetricNamed(fmt.Sprint(randomWalkPrefix, m), metricTags).Value(values[m])
 				sign := float64(1)
 				if rng.Int31n(2) == 1 {
 					sign = float64(-1)
@@ -44,4 +50,43 @@ func main() {
 			}
 		}
 	}
+}
+
+func main() {
+	var (
+		metricsN  int
+		clientsN  int
+		randomTag bool
+		signals   = make(chan os.Signal, 1)
+	)
+	flag.IntVar(&metricsN, "m", 6, "number of metrics")
+	flag.IntVar(&clientsN, "c", 2, "number of clients")
+	flag.BoolVar(&randomTag, "r", false, "add random tag")
+	flag.Parse()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-signals
+		cancel()
+	}()
+
+	client := http.Client{}
+	ensureDashboardExists(client, metricsN)
+
+	var wg sync.WaitGroup
+	for c := 0; c < clientsN; c++ {
+		ci := c
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			shClient := statshouse.NewClient(log.Printf, statshouse.DefaultNetwork, statshouse.DefaultAddr, "")
+			randomWalk(ctx, shClient, statshouse.NamedTags{{"client", fmt.Sprint("client-", ci)}}, metricsN, randomTag)
+		}()
+	}
+
+	<-ctx.Done()
+	log.Print("Stopping...")
+	wg.Wait()
+	log.Print("Stopped")
 }
