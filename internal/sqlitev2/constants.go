19d47683978ae28a9a84d310983223d424a0bfab
Evgeny Martyn <dedok1486@gmail.com>
2024-06-27 14:18:51 +0300

    [sqlitev2] add sqlite error code wrapper (#1283)

--- internal/sqlitev2/constants.go
+++ internal/sqlitev2/constants.go
@@ -1,6 +1,11 @@
 package sqlitev2
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/vkcom/statshouse/internal/sqlitev2/sqlite0"
+	"go.uber.org/multierr"
+)
 
 var (
 	AvoidUnsafe      = false // In the case of some bags
@@ -10,6 +15,32 @@ var (
 	errEnginePanic   = errors.New("sqlite-engine: engine in panic")
 )
 
+var (
+	ErrConstraintUnique     = errors.New("unique_constraint_error")
+	ErrConstraintDatatype   = errors.New("datatype_constraint_error")
+	ErrConstraintPrimarykey = errors.New("primarykey_constraint_error")
+	codeToError             = map[int]error{
+		2067: ErrConstraintUnique,
+		3091: ErrConstraintDatatype,
+		1555: ErrConstraintPrimarykey,
+	}
+)
+
 func IsEngineBrokenError(err error) bool {
 	return err == errEngineBroken || errors.Is(err, errEngineBroken)
+}
+
+func mapSqliteErr(err error) error {
+	if err == nil {
+		return err
+	}
+	var sqliteErr sqlite0.Error
+	if errors.As(err, &sqliteErr) {
+		err1 := codeToError[sqliteErr.Code()]
+		if err1 == nil {
+			return err
+		}
+		return multierr.Append(err, err1)
+	}
+	return err
 }
