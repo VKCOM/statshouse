d9f718c0b89dd04e480f9d0adcf53c3c93aae7e5
Mikhail <m.alpinskiy@vk.team>
2024-07-19 23:41:05 +0300

    Update client test

--- cmd/statshouse-client-test/client_java.go
+++ cmd/statshouse-client-test/client_java.go
@@ -1,25 +1,27 @@
 package main
 
-import "path/filepath"
+import (
+	"path/filepath"
+)
 
 type java struct{ client }
 
-func (*java) localPath() string {
+func (*java) libMain() string {
 	return "src/main/java/com/vk/statshouse/Client.java"
 }
 
-func (*java) remotePath() string {
+func (*java) gitURL() string {
 	return "git@github.com:VKCOM/statshouse-java.git"
 }
 
-func (*java) sourceFileName() string {
+func (*java) testMain() string {
 	return "test.java"
 }
 
-func (c *java) make() error {
-	return c.exec("javac", "-cp", filepath.Join(c.path, "src/main/java/"), "-d", c.temp, "test.java")
+func (client *java) make() error {
+	return client.exec("javac", "-cp", filepath.Join(client.library.rootDir, "src/main/java/"), "-d", client.rootDir, "test.java")
 }
 
-func (c *java) run() error {
-	return c.exec("java", "test")
+func (client *java) run() error {
+	return client.exec("java", "test")
 }
