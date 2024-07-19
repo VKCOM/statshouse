9f498d5d73efd8fb26ee194288e8932b56ec1ff5
Evgeny Martyn <dedok1486@gmail.com>
2024-07-19 13:45:10 +0500

    [sqlitev2] add some docs (#1318)

--- internal/sqlitev2/user_engine.go
+++ internal/sqlitev2/user_engine.go
@@ -2,8 +2,10 @@ package sqlitev2
 
 import "github.com/vkcom/statshouse/internal/vkgo/binlog"
 
+// Все колбэки вызываются в барсик горутине, не стоит в них выполнять долгие операции
 type UserEngine interface {
-	// Движок должен завершить все зпросы и выполнить Close на Engine
+	// Движок должен инициировать завершение работы движка(отправить сигнал на закрытие rpc, engine.close и тд).
+	// Само завершение в этом колбэке выполнять не следует
 	Shutdown()
 	Revert(toOffset int64) // в текущей реализации, требуется действовать аналогично Shutdown
 	ChangeRole(info binlog.ChangeRoleInfo)
