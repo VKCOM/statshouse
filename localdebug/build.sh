ba3288b6d24683d4f4f0ab18f75e4bb7eef1f61d
Grigory Buteyko <hrissan@hrissan.com>
2024-08-23 17:05:36 +0300

    tiny tweaks

--- localdebug/build.sh
+++ localdebug/build.sh
@@ -2,3 +2,9 @@
 set -e
 
 cd .. && make build-main-daemons
+
+# run clickhouse server in docker
+# docker run --network=host --name some-clickhouse-server --ulimit nofile=262144:262144 clickhouse/clickhouse-server
+#
+# run clickhouse client in docker
+# docker exec -it some-clickhouse-server clickhouse-client
