c749ec6a0c2444a1513ad967b0342016d006d1bb
Anton Ulyanov <polecat5@yandex.ru>
2024-10-18 18:42:31 +0300

    StatsHouse UI: fix hash link off

--- statshouse-ui/src/common/getUrlObject.ts
+++ statshouse-ui/src/common/getUrlObject.ts
@@ -1,6 +1,6 @@
 export function getUrlObject(search: string): { hash?: string; search?: string } {
   if (search.length > 5000) {
-    return { hash: search };
+    return { search };
   } else {
     return { search };
   }
