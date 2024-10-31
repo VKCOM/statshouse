dafa948f2cb34abb36dc4c6619c1728e4cc77c99
Sergei Razmetov <razmser@gmail.com>
2024-10-31 19:31:06 +0300

    Write stags into v3 table in agg

--- internal/aggregator/config.go
+++ internal/aggregator/config.go
@@ -22,7 +22,6 @@ type ConfigAggregatorRemote struct {
 	SampleKeys           bool
 	DenyOldAgents        bool
 	MirrorChWrite        bool
-	StringTagProb        float64
 }
 
 type ConfigAggregator struct {
@@ -76,7 +75,6 @@ func DefaultConfigAggregator() ConfigAggregator {
 			SampleKeys:           false,
 			DenyOldAgents:        true,
 			MirrorChWrite:        true,
-			StringTagProb:        0,
 		},
 	}
 }
@@ -90,7 +88,6 @@ func (c *ConfigAggregatorRemote) Bind(f *flag.FlagSet, d ConfigAggregatorRemote,
 		f.BoolVar(&c.SampleKeys, "sample-keys", d.SampleKeys, "Statshouse will sample at key level.")
 		f.BoolVar(&c.DenyOldAgents, "deny-old-agents", d.DenyOldAgents, "Statshouse will ignore data from outdated agents")
 		f.BoolVar(&c.MirrorChWrite, "mirror-ch-writes", d.MirrorChWrite, "Write metrics into both new and old tables")
-		f.Float64Var(&c.StringTagProb, "string-tag-prob", d.StringTagProb, "0 - only mapped tags, 0.01 means replace 1% of mapped tags with fake string")
 	}
 }
 
