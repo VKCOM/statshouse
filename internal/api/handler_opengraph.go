1c2b229ecad43c2d31e3c2ab2004c29d50ceb8e6
Mikhail Alpinskiy <m.alpinskiy@vk.team>
2024-12-24 09:59:02 +0300

    og:image generator now uses original query

--- internal/api/handler_opengraph.go
+++ internal/api/handler_opengraph.go
@@ -23,24 +23,13 @@ type openGraphInfo struct {
 	ImageHeight int
 }
 
-func value(r *http.Request, v string, d string) string {
-	values := r.FormValue(v)
-	if values == "" {
-		values = d
-	}
-
-	return values
-}
-
 func getOpenGraphInfo(r *http.Request, origPath string) *openGraphInfo {
 	if origPath != viewPath {
 		return nil // path does not generate image
 	}
-
 	var (
 		metrics       []string
 		whats         [][]string
-		v             = url.Values{}
 		width, height = plotSize(dataFormatPNG, true, defaultRenderWidth)
 		tab           = 0
 		dashboardID   string
@@ -52,75 +41,23 @@ func getOpenGraphInfo(r *http.Request, origPath string) *openGraphInfo {
 		} else {
 			p = fmt.Sprintf("t%d.", tab)
 		}
-
 		paramMetric := p + ParamMetric
 		// Parse metric
 		metric := r.FormValue(paramMetric)
 		if metric == "" {
 			break
 		}
-
 		var (
-			paramVersion     = p + ParamVersion
-			paramNumResults  = p + ParamNumResults
-			paramQueryBy     = p + ParamQueryBy
-			paramQueryFilter = p + ParamQueryFilter
-			paramTimeShift   = p + ParamTimeShift
-			paramWidthAgg    = p + ParamWidthAgg
-			paramWidth       = p + ParamWidth
-			paramQueryWhat   = p + ParamQueryWhat
-			paramPromQuery   = p + paramPromQuery
-			paramYL          = p + paramYL
-			paramYH          = p + paramYH
+			paramQueryWhat = p + ParamQueryWhat
 		)
-
-		// Build query
-		v.Set(paramMetric, metric)
-		v.Set(paramVersion, value(r, paramVersion, "2"))
-		v[paramNumResults] = r.Form[paramNumResults]
-		v[paramQueryBy] = r.Form[paramQueryBy]
-		v[paramQueryFilter] = r.Form[paramQueryFilter]
-		v[paramTimeShift] = r.Form[paramTimeShift]
-		v[paramPromQuery] = r.Form[paramPromQuery]
-		v[paramYL] = r.Form[paramYL]
-		v[paramYH] = r.Form[paramYH]
-		//-- width
-		widthAgg := r.FormValue(paramWidthAgg)
-		if widthAgg != "" {
-			v.Set(paramWidth, fmt.Sprintf("%ss", widthAgg))
-		} else {
-			v.Set(paramWidth, strconv.Itoa(width))
-		}
 		//-- what
 		what := r.Form[paramQueryWhat]
 		if len(what) == 0 {
 			what = []string{"count_norm"}
 		}
-		v[paramQueryWhat] = what
-
 		// SaveMetric to build title later
 		metrics = append(metrics, metric)
 		whats = append(whats, what)
-	}
-	if dashboardID = r.FormValue(paramDashboardID); len(dashboardID) != 0 {
-		v.Set(paramDashboardID, dashboardID)
-	}
-	if len(v) == 0 {
-		return nil
-	}
-	if t := r.FormValue(ParamFromTime); len(t) != 0 {
-		v.Set(ParamFromTime, t)
-	}
-	if t := r.FormValue(ParamToTime); len(t) != 0 {
-		v.Set(ParamToTime, t)
-	}
-	// forward variables as is
-	for k, s := range r.Form {
-		if strings.HasPrefix(k, "v") {
-			for _, vv := range s {
-				v.Add(k, vv)
-			}
-		}
 	}
 	// Active tab number, total image height
 	tn, err := strconv.Atoi(r.FormValue(paramTabNumber))
@@ -130,13 +67,11 @@ func getOpenGraphInfo(r *http.Request, origPath string) *openGraphInfo {
 		if tn == -1 && 1 < tab {
 			height = int(float32(height) / 2 * float32((tab+1)/2))
 		}
-		v.Set(paramTabNumber, strconv.Itoa(tn))
 	}
-
 	var (
 		u = url.URL{
 			Path:     RoutePrefix + EndpointRender,
-			RawQuery: v.Encode(),
+			RawQuery: r.URL.RawQuery,
 		}
 		title string
 	)
