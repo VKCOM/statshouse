d03615a6f13559c3cabc10bacd291b49f78ef218
Sergei Razmetov <razmser@yandex.ru>
2025-01-16 17:43:28 +0300

    Metrics generator

--- cmd/loadgen/loadgen.go
+++ cmd/loadgen/loadgen.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"github.com/vkcom/statshouse/internal/loadgen"
+	"os"
 )
 
 func main() {
-	loadgen.Main()
+	if len(os.Args) > 1 && os.Args[1] == "agent" {
+		loadgen.RunAgentLoad()
+		return
+	}
+	loadgen.RunLegacy()
 }
