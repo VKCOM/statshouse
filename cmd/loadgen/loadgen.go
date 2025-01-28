c2e449c356cf68885fcc50a71430b04087cc4378
Sergei Razmetov <razmser@gmail.com>
2025-01-28 15:21:20 +0300

    Simple command to enable new pipeline

--- cmd/loadgen/loadgen.go
+++ cmd/loadgen/loadgen.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/vkcom/statshouse/internal/loadgen"
 )
 
+func usage() {
+	fmt.Printf("usage: loadgen client|new-pipeline\n")
+	os.Exit(1)
+}
+
 func main() {
-	if len(os.Args) > 1 && os.Args[1] == "client" {
-		loadgen.RunClientLoad()
-		return
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "client":
+			loadgen.RunClientLoad()
+			return
+		case "new-pipeline":
+			loadgen.RunEnableNewPipeline()
+			return
+		}
 	}
+	usage()
 }
