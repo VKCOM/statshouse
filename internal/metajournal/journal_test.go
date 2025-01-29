e962f1cf3d8d628fe2888af9e208d38fb0ad8482
Grigory Buteyko <hrissan@hrissan.com>
2025-01-29 20:03:02 +0300

    new journal fast, used only in tests for now

--- internal/metajournal/journal_test.go
+++ internal/metajournal/journal_test.go
@@ -8,9 +8,11 @@ package metajournal
 
 import (
 	"encoding/json"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/vkcom/statshouse/internal/data_model"
 	"github.com/vkcom/statshouse/internal/format"
 )
 
@@ -55,4 +57,46 @@ func Benchmark_Load(b *testing.B) {
 			break
 		}
 	}
+}
+
+func Benchmark_LoadFast(b *testing.B) {
+	const testFile = "load_fast.tmp"
+	loader, _, err := getJournalFileLoader("journal.json")
+	require.NoError(b, err)
+	m := MakeMetricsStorage(nil)
+	_ = os.Remove(testFile)
+	fp, err := os.OpenFile(testFile, os.O_CREATE|os.O_RDWR, 0666)
+	require.NoError(b, err)
+
+	j, err := LoadJournalFastFile(fp, data_model.JournalDDOSProtectionTimeout,
+		[]ApplyEvent{m.ApplyEvent})
+	require.NoError(b, err)
+	j.metaLoader = loader
+
+	for {
+		fin, err := j.updateJournalIsFinished(nil)
+		require.NoError(b, err)
+		if fin {
+			break
+		}
+	}
+	require.NoError(b, j.Save())
+	_ = fp.Close()
+	fp, err = os.OpenFile(testFile, os.O_CREATE|os.O_RDWR, 0666)
+	require.NoError(b, err)
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		m2 := MakeMetricsStorage(nil)
+		_, err = LoadJournalFastFile(fp, data_model.JournalDDOSProtectionTimeout,
+			[]ApplyEvent{m2.ApplyEvent})
+		require.NoError(b, err)
+		require.Equal(b, len(m.metricsByID), len(m2.metricsByID))
+		for k, v := range m.metricsByID {
+			v2, ok := m2.metricsByID[k]
+			require.True(b, ok)
+			require.Equal(b, v.Name, v2.Name)
+		}
+	}
+	_ = os.Remove(testFile)
 }
