25d1d8a03577cec1634f2914dfffd0f30823e88f
Sergei Razmetov <razmser@gmail.com>
2025-01-31 15:52:58 +0300

    Bump copyright to 2025

--- statshouse-ui/src/store/dev/selectors.ts
+++ statshouse-ui/src/store/dev/selectors.ts
@@ -1,4 +1,4 @@
-// Copyright 2022 V Kontakte LLC
+// Copyright 2025 V Kontakte LLC
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
