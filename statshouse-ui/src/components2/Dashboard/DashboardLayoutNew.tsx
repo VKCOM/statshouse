0ab2c583b6c0da1889d2f5ceb622b5101e0a421e
Vasiliy Trushin <v.trushin@vk.team>
2025-02-27 19:18:50 +0300

    style fixes

--- statshouse-ui/src/components2/Dashboard/DashboardLayoutNew.tsx
+++ statshouse-ui/src/components2/Dashboard/DashboardLayoutNew.tsx
@@ -417,8 +417,10 @@ export const DashboardLayoutNew = memo(function DashboardLayoutNew({ className }
                       >
                         <PlotView
                           className={cn(
-                            isDashboardEditAllowed && css.pointerEventsNone,
-                            isDashboardEditAllowed && isNotMobile && 'position-relative overflow-hidden w-100 h-100'
+                            isDashboardEditAllowed && [
+                              css.pointerEventsNone,
+                              'position-relative overflow-hidden w-100 h-100',
+                            ]
                           )}
                           key={`plot-${plot}`}
                           plotKey={plot}
