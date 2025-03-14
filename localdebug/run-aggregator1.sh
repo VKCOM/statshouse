758d93fc76d80450e4853e3e203e7ceb1d6f9ebc
Sergei Razmetov <razmser@gmail.com>
2025-03-14 12:30:31 +0300

    Fix run-agg scripts

--- localdebug/run-aggregator1.sh
+++ localdebug/run-aggregator1.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 
-mkdir -p cache/aggregator/
-../target/statshouse-agg --auto-create-default-namespace --agg-addr=127.0.0.1:13336,127.0.0.1:13346,127.0.0.1:13356 --local-replica=1 --cluster=local_test_cluster --kh=127.0.0.1:8123 --metadata-addr "127.0.0.1:2442" --cache-dir=cache/aggregator1 "$@"
+mkdir -p cache/aggregator1/
+../target/statshouse-agg --auto-create --auto-create-default-namespace --agg-addr=127.0.0.1:13336,127.0.0.1:13346,127.0.0.1:13356 --local-replica=1 --cluster=statlogs2 --kh=127.0.0.1:8123 --metadata-addr "127.0.0.1:2442" --cache-dir=cache/aggregator1 "$@"
