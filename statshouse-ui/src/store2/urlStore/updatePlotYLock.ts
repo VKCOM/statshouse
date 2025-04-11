941034e931d46e69312049b67fe413ba3492cb95
Anton Ulyanov <polecat5@yandex.ru>
2025-04-11 14:45:55 +0300

    StatsHouse UI: fix y lock

--- statshouse-ui/src/store2/urlStore/updatePlotYLock.ts
+++ statshouse-ui/src/store2/urlStore/updatePlotYLock.ts
@@ -4,14 +4,16 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-import type { PlotKey } from '../../url2';
+import type { PlotKey } from '@/url2';
 import type { ProduceUpdate } from '../helpers';
-import type { StatsHouseStore } from '../statsHouseStore';
+import type { StatsHouseStore } from '@/store2';
+import { getMinMaxY } from '@/store2/urlStore/getMinMaxY';
+import { usePlotsDataStore } from '@/store2/plotDataStore';
 
 export function updatePlotYLock(
   plotKey: PlotKey,
   status: boolean,
-  yLock: { min: number; max: number }
+  yLock?: { min: number; max: number }
 ): ProduceUpdate<StatsHouseStore> {
   return (state) => {
     const plot = state.params.plots[plotKey];
@@ -20,6 +22,9 @@ export function updatePlotYLock(
       const prevStatus = prevYLock.max !== 0 || prevYLock.min !== 0;
       if (prevStatus !== status || prevYLock.max !== (yLock?.max ?? 0) || prevYLock.min !== (yLock?.min ?? 0)) {
         if (status) {
+          if (!yLock) {
+            yLock = getMinMaxY(usePlotsDataStore.getState().plotsData[plotKey]);
+          }
           plot.yLock = yLock;
         } else {
           plot.yLock = { min: 0, max: 0 };
