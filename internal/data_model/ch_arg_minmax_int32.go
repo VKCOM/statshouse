8d8f1bbd37cde93c8652e75cdf6b7a35155b6c80
Sergei Razmetov <razmser@gmail.com>
2025-08-07 00:42:07 +0300

    feat(data_model): implement ArgMinMax serialization
    Add ArgMinMax data structures for handling min/max value tracking
    in v3 format. This includes:
    
    - ChArgMinMaxInt32 and ChArgMinMaxString implementations
    - Marshaling and unmarshaling methods for ClickHouse binary format
    - Comprehensive unit tests and integration tests
    - Support for both int32 and string value types
    
    The ArgMinMax implementation enables efficient tracking of minimum
    and maximum values with associated metadata in the new table format.

---
+++ internal/data_model/ch_arg_minmax_int32.go
@@ -0,0 +1,91 @@
+// Copyright 2025 V Kontakte LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package data_model
+
+import (
+	"encoding/binary"
+	"io"
+	"math"
+)
+
+type ArgMinMaxInt32Float32 struct {
+	Arg int32
+	Val float32
+}
+
+type ArgMinInt32Float32 struct {
+	ArgMinMaxInt32Float32
+}
+
+type ArgMaxInt32Float32 struct {
+	ArgMinMaxInt32Float32
+}
+
+func (arg *ArgMinMaxInt32Float32) ReadFrom(r io.ByteReader) error {
+	hasArg, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+	if hasArg != 0 {
+		arg.Arg, err = readInt32LE(r)
+		if err != nil {
+			return err
+		}
+	}
+	valueFlag, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+	if valueFlag != 0 {
+		arg.Val, err = readFloat32LE(r)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (arg *ArgMinInt32Float32) Merge(rhs ArgMinInt32Float32) {
+	if rhs.Val < arg.Val {
+		*arg = rhs
+	}
+}
+
+func (arg *ArgMaxInt32Float32) Merge(rhs ArgMaxInt32Float32) {
+	if arg.Val < rhs.Val {
+		*arg = rhs
+	}
+}
+
+// MarshalBinary serializes ArgMinMaxInt32Float32 to ClickHouse RowBinary format
+func (arg *ArgMinMaxInt32Float32) MarshalAppend(buf []byte) []byte {
+	if arg.Arg != 0 {
+		buf = append(buf, 1)
+		var tmp [4]byte
+		binary.LittleEndian.PutUint32(tmp[:], uint32(arg.Arg))
+		buf = append(buf, tmp[:]...)
+	} else {
+		buf = append(buf, 0)
+	}
+	if arg.Val != 0 {
+		buf = append(buf, 1)
+		var tmp [4]byte
+		binary.LittleEndian.PutUint32(tmp[:], math.Float32bits(arg.Val))
+		buf = append(buf, tmp[:]...)
+	} else {
+		buf = append(buf, 0)
+	}
+	return buf
+}
+
+func readInt32LE(r io.ByteReader) (int32, error) {
+	bits, err := readUint32LE(r)
+	if err != nil {
+		return 0, err
+	}
+	return int32(bits), nil
+}
