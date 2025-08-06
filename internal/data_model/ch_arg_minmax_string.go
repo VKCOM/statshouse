f60a5a6d1a9c732fc389a7321cc93e098443d259
Sergei Razmetov <razmser@gmail.com>
2025-08-07 00:42:07 +0300

    feat(data_model): implement ArgMinMax serialization
    Add ArgMinMax data structures for handling min/max value tracking
    in v3 format. This includes:
    
    - ChArgMinMaxInt32 and ChArgMinMaxString implementations
    - Marshaling and unmarshaling methods for ClickHouse binary format
    - Comprehensive unit tests and integration tests
    - Support for both int32 and string value types
    
    The ArgMinMax implementation enables efficient tracking of minimum
    and maximum values with associated metadata in the new table format.

---
+++ internal/data_model/ch_arg_minmax_string.go
@@ -0,0 +1,131 @@
+// Copyright 2025 V Kontakte LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package data_model
+
+import (
+	"encoding/binary"
+	"io"
+	"math"
+	"slices"
+)
+
+type ArgMinMaxStringFloat32 struct {
+	AsString string
+	AsInt32  int32
+	Val      float32
+}
+
+type ArgMinStringFloat32 struct {
+	ArgMinMaxStringFloat32
+}
+
+type ArgMaxStringFloat32 struct {
+	ArgMinMaxStringFloat32
+}
+
+func (arg *ArgMinMaxStringFloat32) ReadFrom(r io.ByteReader, buf []byte) ([]byte, error) {
+	buf = slices.Grow(buf, 6)[:6]
+	// read string
+	len, err := readUint32LE(r)
+	if err != nil {
+		return buf, err
+	}
+	// 0xffffffff is a special value that means "no string"
+	if len != 0xffffffff && len > 0 {
+		strFlag, err := r.ReadByte()
+		if err != nil {
+			return buf, err
+		}
+		if strFlag == 1 {
+			buf = slices.Grow(buf, int(len-1))[:len-1]
+			lastNonNull := 0
+			for i := 0; i < int(len-1); i++ {
+				buf[i], err = r.ReadByte()
+				if err != nil {
+					return buf, err
+				}
+				if buf[i] != 0 {
+					lastNonNull = i
+				}
+			}
+			arg.AsString = string(buf[:lastNonNull+1])
+		} else {
+			arg.AsInt32, err = readInt32LE(r)
+			if err != nil {
+				return buf, err
+			}
+		}
+	}
+	// read value
+	valueFlag, err := r.ReadByte()
+	if err != nil {
+		return buf, err
+	}
+	if valueFlag != 0 {
+		val, err := readFloat32LE(r)
+		if err != nil {
+			return buf, err
+		}
+		arg.Val = val
+	}
+	return buf, nil
+}
+
+func (arg *ArgMinStringFloat32) Merge(rhs ArgMinStringFloat32) {
+	if arg.Empty() {
+		*arg = rhs
+	} else if rhs.Val < arg.Val {
+		*arg = rhs
+	}
+}
+
+func (arg *ArgMaxStringFloat32) Merge(rhs ArgMaxStringFloat32) {
+	if arg.Empty() {
+		*arg = rhs
+	} else if arg.Val < rhs.Val {
+		*arg = rhs
+	}
+}
+
+// TODO: remove if unused
+func (arg *ArgMinMaxStringFloat32) Merge(rhs ArgMinMaxStringFloat32, x int) {
+	if arg.Empty() {
+		*arg = rhs
+	} else if x == 0 {
+		// min
+		if rhs.Val < arg.Val {
+			*arg = rhs
+		}
+	} else {
+		// max
+		if arg.Val < rhs.Val {
+			*arg = rhs
+		}
+	}
+}
+
+func (arg *ArgMinMaxStringFloat32) Empty() bool {
+	return arg.AsInt32 == 0 && arg.AsString == ""
+}
+
+func (arg *ArgMinMaxStringFloat32) MarshallAppend(buf []byte) []byte {
+	if arg.AsString != "" {
+		dataLen := uint32(len(arg.AsString) + 2)
+		buf = binary.LittleEndian.AppendUint32(buf, dataLen)
+		buf = append(buf, 1) // string marker
+		buf = append(buf, []byte(arg.AsString)...)
+		buf = append(buf, 0) // for some reason ClickHouse likes to add string terminator
+	} else {
+		dataLen := uint32(5) // sizeof(int32) + 1
+		buf = binary.LittleEndian.AppendUint32(buf, dataLen)
+		buf = append(buf, 0) // int marker
+		buf = binary.LittleEndian.AppendUint32(buf, uint32(arg.AsInt32))
+	}
+	buf = append(buf, 1) // has value flag
+	buf = binary.LittleEndian.AppendUint32(buf, math.Float32bits(arg.Val))
+	return buf
+}
---
+++ internal/data_model/ch_arg_minmax_test.go
@@ -0,0 +1,70 @@
+// Copyright 2025 V Kontakte LLC
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package data_model
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"pgregory.net/rapid"
+)
+
+func TestArgMinMaxInt32Float32_Roundtrip(t *testing.T) {
+	rapid.Check(t, func(t *rapid.T) {
+		arg := ArgMinMaxInt32Float32{
+			Arg: rapid.Int32().Draw(t, "arg"),
+			Val: rapid.Float32().Draw(t, "val"),
+		}
+		buf := arg.MarshalAppend(nil)
+		r := bytes.NewReader(buf)
+		var out ArgMinMaxInt32Float32
+		require.NoError(t, out.ReadFrom(r))
+		require.Equal(t, arg.Arg, out.Arg)
+		require.Equal(t, arg.Val, out.Val)
+	})
+}
+
+func TestArgMinMaxStringFloat32_StringArg_Roundtrip(t *testing.T) {
+	scratch := make([]byte, 32)
+	rapid.Check(t, func(t *rapid.T) {
+		asString := rapid.String().Draw(t, "asString")
+		val := rapid.Float32().Draw(t, "val")
+		arg := ArgMinMaxStringFloat32{
+			AsString: asString,
+			Val:      val,
+		}
+		buf := arg.MarshallAppend(nil)
+		r := bytes.NewReader(buf)
+		var out ArgMinMaxStringFloat32
+		var err error
+		scratch, err = out.ReadFrom(r, scratch[:0])
+		require.NoError(t, err)
+		require.Equal(t, arg.AsString, out.AsString)
+		require.Equal(t, arg.Val, out.Val)
+	})
+}
+
+func TestArgMinMaxStringFloat32_IntArg_Roundtrip(t *testing.T) {
+	scratch := make([]byte, 32)
+	rapid.Check(t, func(t *rapid.T) {
+		asInt := rapid.Int32().Draw(t, "arg")
+		val := rapid.Float32().Draw(t, "val")
+		arg := ArgMinMaxStringFloat32{
+			AsInt32: asInt,
+			Val:     val,
+		}
+		buf := arg.MarshallAppend(nil)
+		r := bytes.NewReader(buf)
+		var out ArgMinMaxStringFloat32
+		var err error
+		scratch, err = out.ReadFrom(r, scratch[:0])
+		require.NoError(t, err)
+		require.Equal(t, arg.Val, out.Val)
+		require.Equal(t, arg.AsInt32, out.AsInt32)
+	})
+}
