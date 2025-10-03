3b268d48d615fe864f051266678a75c37162e9cb
Grigory Buteyko <hrissan@hrissan.com>
2025-10-03 14:02:06 +0300

    remove almost never used optimization for custom resolution metrics with tags_hash

--- internal/sharding/sharding_test.go
+++ internal/sharding/sharding_test.go
@@ -3,8 +3,6 @@ package sharding
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/VKCOM/statshouse/internal/data_model"
 	"github.com/VKCOM/statshouse/internal/format"
 )
@@ -18,7 +16,6 @@ func TestShard(t *testing.T) {
 		key       data_model.Key
 		meta      *format.MetricMetaValue
 		numShards int
-		byMetric  bool
 	}
 	tests := []struct {
 		name          string
@@ -30,61 +27,48 @@ func TestShard(t *testing.T) {
 			key:       metric1,
 			meta:      &format.MetricMetaValue{Name: "a", ShardStrategy: format.ShardByTagsHash},
 			numShards: 16,
-			byMetric:  false, // meta has priority
 		}, 13},
 		{"ok-by-tags-hash-2", args{
 			key:       metric2,
 			meta:      &format.MetricMetaValue{Name: "a", ShardStrategy: format.ShardByTagsHash},
 			numShards: 16,
-			byMetric:  false, // meta has priority
 		}, 1},
 		{"ok-by-tags-builtin", args{
 			key:       metricBuiltin,
 			meta:      &format.MetricMetaValue{Name: "a", ShardStrategy: format.ShardByTagsHash},
 			numShards: 16,
-			byMetric:  false, // meta has priority
 		}, 11},
 
 		{"ok-fixed-shard", args{
 			key:       metric1,
 			meta:      &format.MetricMetaValue{Name: "a", ShardStrategy: format.ShardFixed, ShardNum: 3},
 			numShards: 16,
-			byMetric:  true,
 		}, 3},
 		{"ok-fixed-builtin", args{
 			key:       metricBuiltin,
 			meta:      &format.MetricMetaValue{Name: "a", ShardStrategy: format.ShardFixed, ShardNum: 0},
 			numShards: 16,
-			byMetric:  true,
 		}, 0},
 
 		{"ok-by-metric-id", args{
 			key:       metric1,
 			meta:      &format.MetricMetaValue{Name: "a", ShardStrategy: format.ShardByMetric},
 			numShards: 16,
-			byMetric:  true,
 		}, 1},
 
 		{"new-sharding-true-user-metric", args{
 			key:       metric1,
 			meta:      &format.MetricMetaValue{Name: "a"},
 			numShards: 16,
-			byMetric:  true,
 		}, 1},
 	}
 
 	scratch := make([]byte, 0)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotShard, byMetric, _, gotHash := Shard(&tt.args.key, tt.args.meta, tt.args.numShards, uint32(tt.args.numShards), &scratch)
+			gotShard, _ := Shard(&tt.args.key, tt.args.meta, tt.args.numShards, uint32(tt.args.numShards), &scratch)
 			if gotShard != tt.expectedShard {
 				t.Errorf("Sharding() = %v, want %v", gotShard, tt.expectedShard)
-			}
-			assert.Equal(t, tt.args.byMetric, byMetric)
-			if byMetric {
-				assert.Zero(t, gotHash)
-			} else {
-				assert.NotZero(t, gotHash)
 			}
 		})
 	}
