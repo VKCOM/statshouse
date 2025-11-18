9ea2d7ca6dece27143f3dced0ff83c02e09762fa
Рустам Назаров <61919245+Rustamchuk@users.noreply.github.com>
2025-11-18 17:30:45 +0300

    builtin metric meta fix (#2067)
    Co-authored-by: ru.nazarov <ru.nazarov@vkteam.ru>

--- internal/format/builtin.go
+++ internal/format/builtin.go
@@ -157,6 +157,8 @@ var (
 		BuiltinMetricIDMigrationLog:             BuiltinMetricMetaMigrationLog,
 		-142:                                    BuiltinMetricMetaAPISelectOSCPUVirtualTime,
 		-143:                                    BuiltinMetricMetaAPIRateLimit,
+		-144:                                    BuiltinMetricMetaMappingClientWaits,
+		-145:                                    BuiltinMetricMetaMappingCacheVersion,
 	}
 
 	// BuiltInGroupDefault can be overridden by journal, don't use directly
