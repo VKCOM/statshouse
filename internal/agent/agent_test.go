621739a604b35bcdccf0509c59c84d07fc34cc59
ru.nazarov <ru.nazarov@vkteam.ru>
2026-04-07 11:37:25 +0300

    fix test

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -10,6 +10,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"os"
+	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -256,6 +257,79 @@ func Benchmark_AgentApplyMetric(b *testing.B) {
 		h.Key = randKey(rng, nowUnix, 1)
 		agent.ApplyMetric(m, h, &scratch)
 	}
+}
+
+// Benchmark_SampleBucketMetricBudgetsFromAgg-14    	     657	   1805336 ns/op	  624347 B/op	    1322 allocs/op
+func Benchmark_SampleBucketMetricBudgetsFromAgg(b *testing.B) {
+	startTime := time.Unix(1000*24*3600, 0) // arbitrary deterministic test time
+	nowUnix := uint32(startTime.Unix())
+	config := DefaultConfig()
+	config.SampleBudget = 40_000
+	config.MinSampleBudget = 4000
+	config.EnableBudgetFromAgg = true
+	agent := makeAgent(config, nowUnix)
+	agent.shardByMetricCount = uint32(len(agent.Shards))
+	shard := agent.Shards[0]
+	shard.metricBudgetsFromAgg = data_model.NewExpDecay(config.BudgetDecayHalfLife)
+
+	const metricN = 256
+	const itemN = 8_192
+
+	keys, items := makeBenchmarkSampleItems(nowUnix, itemN, metricN)
+	for metric := int32(1); metric <= metricN; metric++ {
+		shard.metricBudgetsFromAgg.MergeMax(metric, 64)
+	}
+
+	rng := rand.New()
+	sb := tlstatshouse.SourceBucket3{}
+	buffers := data_model.SamplerBuffers{}
+	budgetScratch := make(map[int32]int64, metricN)
+	scratch := make([]byte, 0, 64*1024)
+	var sampledCount uint64
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		bucket := &data_model.MetricsBucket{
+			Time: nowUnix,
+			MultiItemMap: data_model.MultiItemMap{
+				MultiItems: make(map[string]*data_model.MultiItem, len(items)),
+			},
+		}
+		for i := range items {
+			bucket.MultiItems[keys[i]] = items[i]
+		}
+		sb = tlstatshouse.SourceBucket3{}
+		b.StartTimer()
+
+		buffers, scratch = shard.sampleBucket(bucket, &sb, buffers, scratch, budgetScratch, rng)
+		sampledCount += uint64(len(sb.Metrics) + len(sb.SampleFactors))
+	}
+	sideEffect += sampledCount
+}
+
+func makeBenchmarkSampleItems(ts uint32, itemN int, metricN int32) ([]string, []*data_model.MultiItem) {
+	keys := make([]string, itemN)
+	items := make([]*data_model.MultiItem, itemN)
+	for i := 0; i < itemN; i++ {
+		metricID := int32(i%int(metricN)) + 1
+		item := &data_model.MultiItem{
+			Key: data_model.Key{
+				Timestamp: ts,
+				Metric:    metricID,
+			},
+			MetricMeta: &format.MetricMetaValue{
+				MetricID:            metricID,
+				EffectiveResolution: 1,
+				EffectiveWeight:     1,
+			},
+		}
+		item.Tail.Value.AddCounter(1)
+		items[i] = item
+		keys[i] = strconv.Itoa(i)
+	}
+	return keys, items
 }
 
 func randKey(rng *rand.Rand, ts uint32, metricOffset int32) data_model.Key {
