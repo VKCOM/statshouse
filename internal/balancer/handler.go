ef39a158059c3e189123aaa689a012fa79aef9ca
ru.nazarov <ru.nazarov@vkteam.ru>
2026-07-03 21:57:32 +0300

    make alone batch balancer sender

--- internal/balancer/handler.go
+++ internal/balancer/handler.go
@@ -13,20 +13,17 @@ import (
 )
 
 const (
-	pktHeadLen   = 4
-	pktBodyMax   = receiver.MaxTCPFrameBody
-	pktFrameMax  = pktHeadLen + pktBodyMax
-	sendInterval = 1 * time.Second // guaranteed send for small traffic
+	pktHeadLen  = 4
+	pktBodyMax  = receiver.MaxTCPFrameBody
+	pktFrameMax = pktHeadLen + pktBodyMax
 )
 
 type handler struct {
 	egress *Egress
 	mu     sync.Mutex
 
-	pkt      []byte
-	lastSend time.Time
+	pkt []byte
 
-	sendInterval   time.Duration
 	reportInterval time.Duration
 	stop           chan struct{}
 }
@@ -34,12 +31,10 @@ type handler struct {
 func newHandler(e *Egress) *handler {
 	h := &handler{
 		egress:         e,
-		sendInterval:   sendInterval,
 		reportInterval: 30 * time.Second,
 		pkt:            make([]byte, pktHeadLen, pktFrameMax),
 		stop:           make(chan struct{}),
 	}
-	go h.sendLoop()
 	go h.reportLoop()
 	return h
 }
@@ -49,7 +44,6 @@ func (h *handler) Close() {
 	defer h.mu.Unlock()
 
 	if h.egress != nil {
-		h.flushLocked()
 		es := h.egress.Stats()
 		log.Printf("balancer stats: fwd=%d drop=%d reconnect_err=%d dns_err=%d write_err=%d",
 			es.ForwardedPackets, es.DroppedPackets,
@@ -63,43 +57,16 @@ func (h *handler) HandleMetrics(data_model.HandlerArgs) (r data_model.MappedMetr
 func (h *handler) HandleMetricsBatchRaw(pkt []byte) error {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-	if len(h.pkt)+len(pkt) > pktFrameMax { // ok for tcp
-		h.flushLocked()
+	if len(pkt) <= 0 {
+		return nil
 	}
-	h.pkt = append(h.pkt, pkt...)
+	h.pkt = append(h.pkt[:pktHeadLen], pkt...)
+	binary.LittleEndian.PutUint32(h.pkt[:pktHeadLen], uint32(len(pkt)))
+	h.pkt = h.egress.WritePacketLocked(h.pkt)[:pktHeadLen] // swap packet
 	return nil
 }
 
-func (h *handler) flushLocked() {
-	if len(h.pkt) <= pktHeadLen {
-		return
-	}
-	binary.LittleEndian.PutUint32(h.pkt[:pktHeadLen], uint32(len(h.pkt)-pktHeadLen))
-	h.pkt = h.egress.WritePacketLocked(h.pkt)[:pktHeadLen] // swap packet
-	h.lastSend = time.Now()
-}
-
 func (h *handler) HandleParseError(_ []byte, _ error) {}
-
-func (h *handler) sendLoop() {
-	t := time.NewTicker(h.sendInterval)
-	defer t.Stop()
-	for {
-		select {
-		case <-h.stop:
-			return
-		case now := <-t.C:
-			func() {
-				h.mu.Lock()
-				defer h.mu.Unlock()
-				if !h.lastSend.IsZero() && now.Sub(h.lastSend) < h.sendInterval {
-					return
-				}
-				h.flushLocked()
-			}()
-		}
-	}
-}
 
 func (h *handler) reportLoop() {
 	t := time.NewTicker(h.reportInterval)
